3cf73e087269bae116cec2d7dc9dce3238abe52b
adnan2912 <142055954+adnan2912@users.noreply.github.com>
2023-08-12 02:16:02 +0530

    Update code.sql

--- code.sql
+++ code.sql
@@ -1,12 +1,3 @@
 //sql file
 
- \resumeProject
-      {Op} %Project Name
-      {Project description:ncnkcjnk} %Project Name, Location Name
-      {Event dates} %Event Dates
 
-      \resumeItemListStart
-        \item {Tools \& technologies used: xxx,xxx}
-        \item {More description on the project(The output you achieved by working on the project)}
-    \resumeItemListEnd
-    \vspace{-2mm}
